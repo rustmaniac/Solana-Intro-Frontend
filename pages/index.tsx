0dd016907a2105ae4726e41244721985712d98d5
JanMarkis <janmarkirsten91@outlook.de>
2023-07-04 19:19:54 +0900

    start point of solana next

---
+++ pages/index.tsx
@@ -0,0 +1,30 @@
+import type { NextPage } from 'next'
+import { useState } from 'react'
+import styles from '../styles/Home.module.css'
+import AddressForm from '../components/AddressForm'
+import * as Web3 from '@solana/web3.js'
+
+const Home: NextPage = () => {
+  const [balance, setBalance] = useState(0)
+  const [address, setAddress] = useState('')
+
+  const addressSubmittedHandler = (address: string) => {
+    setAddress(address)
+    setBalance(1000)
+  }
+
+  return (
+    <div className={styles.App}>
+      <header className={styles.AppHeader}>
+        <p>
+          Start Your Solana Journey
+        </p>
+        <AddressForm handler={addressSubmittedHandler} />
+        <p>{`Address: ${address}`}</p>
+        <p>{`Balance: ${balance} SOL`}</p>
+      </header>
+    </div>
+  )
+}
+
+export default Home
